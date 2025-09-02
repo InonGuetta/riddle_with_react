c8b1876930ddb0fb770db9696895fd7e9765a93c
Inon Guetta <inon16guetta@gmail.com>
2025-09-02 17:43:22 +0300

    fix_todeploy

--- riddle_react/src/App.tsx
+++ riddle_react/src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
+    <h1>base strecture</h1>
     </>
   )
 }
