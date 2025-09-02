b3dfef0d58da2e2a9e70ea84ebebf4f65e9f013e
Inon Guetta <inon16guetta@gmail.com>
2025-09-02 17:16:55 +0300

    base_strecture

---
+++ riddle_react/src/components/pages/PageFinishPlayers.tsx
@@ -0,0 +1,6 @@
+export default function PageFinishPlayers(){
+    return(
+        <>
+        </>
+    )
+}
---
+++ riddle_react/src/components/pages/ShowRiddle.tsx
@@ -0,0 +1,6 @@
+export default function ShowRiddle(){
+    return(
+        <>
+        </>
+    )
+}
