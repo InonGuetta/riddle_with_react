47a2be9fedcf54181efe8bad24b98110f74a709a
Inon Guetta <inon16guetta@gmail.com>
2025-09-06 21:03:25 +0300

    finish_feature_of_send_player

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import DeleteRiddle from './components/pages/DeleteRiddle.tsx';
-import InsertRiddle from './components/pages/InsertRiddle.tsx';
-import PageFinishPlayers from './components/pages/PageFinishPlayers.tsx';
 import ShowRiddle from './components/pages/ShowRiddle.tsx';
 import SortedPlayrs from './components/pages/SortedPlayers.tsx';
 import StartGame from './components/pages/StartGame.tsx';
-import UpdateRiddle from './components/pages/UpdateRiddle.tsx';
-import PlayersPage from './components/pages/PlayersPage.tsx';
 import { BrowserRouter, Route, Routes} from 'react-router';
 
 
@@ -17,12 +12,8 @@ createRoot(document.getElementById('root')!).render(
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/startgame' element={<StartGame/>}/>
-      <Route path='/delete' element={<DeleteRiddle/>}/>
-      <Route path='/finish' element={<PageFinishPlayers/>}/>
       <Route path='/allriddle' element={<ShowRiddle/>}/>
       <Route path='/sortplayer' element={<SortedPlayrs/>}/>
-      <Route path='/players' element={<PlayersPage/>}/>
-      <Route path='/update' element={<UpdateRiddle/>}/>
     </Routes>
     </BrowserRouter>
 )
